06037ca769cf26b3b8d5ec06433848c30902c85c
Alexandros Lagios <alexandroslagios@gmail.com>
2020-03-06 22:07:29 +0200

    Initial commit

---
+++ src/main/java/uk/co/exus/reportingtool/ReportingToolApplication.java
@@ -0,0 +1,13 @@
+package uk.co.exus.reportingtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportingToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportingToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uk/co/exus/reportingtool/ReportingToolApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.co.exus.reportingtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportingToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
